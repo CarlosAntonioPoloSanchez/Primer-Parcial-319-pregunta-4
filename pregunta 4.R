e2ecda64ea8f4de1f8c0a59c00d38e4ac37ca3e1
CarlosAntonioPoloSanchez <105763178+CarlosAntonioPoloSanchez@users.noreply.github.com>
2022-05-18 10:17:16 -0400

    Add files via upload

---
+++ pregunta 4.R
@@ -0,0 +1,7 @@
+df <- data.frame(
+  "corazon" = c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,"j","j", "q","q", "k","k"), 
+  "trebol" = c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,"j","j", "q","q", "k","k"), 
+  "pica" = c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,"j","j", "q","q", "k","k"),
+  "diamante" = c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,"j","j", "q","q", "k","k"),
+  "color"= c("rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro","rojo", "negro",))
+df
